90a5392566609b19002e3fc9a65c60ffe14ebbc9
juwan-25 <s2109@e-mirim.hs.kr>
2022-03-17 10:28:06 +0900

    배열 활용

--- codingtest2022/sumAvg.c
+++ codingtest2022/sumAvg.c
@@ -6,14 +6,16 @@ int main()
 	int num[10];
 	int sum = 0;
 	double avg;
-	for (int i = 0; i < 10; i++) {
-		num[i] = rand()/100;
+	for (int i = 0; i < sizeof(num)/sizeof(int); i++) {
+		num[i] = (rand()/100)+1;
+		printf("%d ", num[i]);
 	}
+	printf("\n\n");
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < sizeof(num) / sizeof(int); i++)
 		sum += num[i];
 
-	avg = (double)sum / 10;
+	avg = (double)sum / (sizeof(num) / sizeof(int));
 	
 	printf("ÇÕ°è : %d Æò±Õ : %lf", sum, avg);
  	return 0;
