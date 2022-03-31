24c419a7eb46759f3c876becd29a43c10fcf3f54
juwan-25 <s2109@e-mirim.hs.kr>
2022-03-31 09:40:01 +0900

    문자열 활용

---
+++ codingtest2022/CharArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	char color[5][10] = { "red", "green", "blue", "brown", "cyan" };
+	//문자열 단위
+	for (int i = 0; i < 5; i++)
+		printf("%s\n", color[i]);
+	printf("\n");
+
+	//문자 단위
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j< 10; j++) {
+			printf("%c", color[i][j]);
+		}
+		printf("\n");
+	}
+
+ 	return 0;
+}
